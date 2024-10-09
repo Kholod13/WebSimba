3bb43adaf5ad10221dc3f89f3d81bf17521d657f
Kholod13 <111706893+Kholod13@users.noreply.github.com>
2024-10-09 21:00:53 +0200

    upload hw

---
+++ WebSimba/WebSimba/Migrations/20241009185630_init database1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebSimba.Migrations
+{
+    /// <inheritdoc />
+    public partial class initdatabase1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
